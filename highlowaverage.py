dd9bdf273ec14336db00ac6835806ef255fa7629
PhoenixxPlays <wolfscopez69@gmail.com>
2026-03-08 17:19:46 -0400

    Update highlowaverage.py

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,27 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file:
+
+numFile = open("numbers.txt" , 'r')
+
+numList = []
+
+for curLine in numFile:
+    print(int(curLine))
+    numList.append(int(curLine))
+totalNum = len(numList)
+sumNumbers = sum(numList)
+average = sumNumbers / totalNum
+highestNum = max(numList)
+lowestNum = min(numList)
+    
+numFile.close
+
+print(f"The total amount of numbers is {totalNum}")
+print(f"The sum of all the numbers is {sumNumbers}")
+print(f"The average of all the numbers is {average}")
+print(f"The highest number is {highestNum}")
+print (f"The lowest number is {lowestNum}")
+
+
+
